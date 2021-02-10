4ece59ce3fa969e3d6ef26dcfaccd7bdddbb7558
bwallace <byron.wallace@gmail.com>
2021-02-10 16:56:04 -0500

    progress

--- screening_model.py
+++ screening_model.py
@@ -1,9 +1,14 @@
 '''
 RobotScreener
 '''
+import os 
+
+from typing import Type, Tuple
+
+import numpy as np 
 
 import torch
-from torch.utils.data import IterableDataset, DataLoader
+from torch.utils.data import Dataset, IterableDataset, DataLoader
 
 import transformers
 from transformers import AdamW
@@ -12,21 +17,52 @@ from transformers import RobertaForSequenceClassification, RobertaTokenizer, Pre
 
 device = torch.device("cpu") #torch.device('cuda')
 
+WEIGHTS_PATH = "saved_model_weights"
 
-def train(dataset: IterableDataset) -> bool:
+def train(dl: DataLoader, epochs: int = 1) -> Tuple[Type[torch.nn.Module], Type[transformers.PreTrainedTokenizer]]:
+
+    ''' model and optimizer ''' 
     tokenizer = RobertaTokenizer.from_pretrained("allenai/biomed_roberta_base") 
     model     = RobertaForSequenceClassification.from_pretrained("allenai/biomed_roberta_base", 
-                                                                 num_labels=n_labels).to(device=device) 
-    
-    
+                                                                 num_labels=2).to(device=device) 
     optimizer = AdamW(model.parameters())
 
+    best_val = np.inf
+    for epoch in range(epochs):
+        print(f"on epoch {epoch}.")
+        model.train()
 
-def train_and_save(titles, abstracts, labels, uuid):
-    pass
+        for (X, y) in dl:
+            optimizer.zero_grad()
 
+            batch_X_tensor = tokenizer.batch_encode_plus(X, max_length=512, 
+                                                        add_special_tokens=True, 
+                                                        pad_to_max_length=True)
+            batch_y_tensor = torch.tensor(y)
+            model_outputs = model(torch.tensor(batch_X_tensor['input_ids']).to(device=device), 
+                              attention_mask=torch.tensor(batch_X_tensor['attention_mask']).to(device=device), 
+                              labels=batch_y_tensor.to(device=device))
+            
+            
+            model_outputs['loss'].backward()
+            optimizer.step()
 
-class SRDataset():
+    return model, tokenizer
+    
+def train_and_save(sr_dataset: Dataset, uuid: str, batch_size: int = 16, epochs: int = 1) -> bool:
+    dl = DataLoader(sr_dataset, batch_size=batch_size)
+    model, tokenizer = train(dl, epochs=epochs)
+
+    out_path = os.path.join(WEIGHTS_PATH, uuid)
+    try: 
+        print(f"dumping model weights to {out_path}...")
+        torch.save(model.state_dict(), out_path)
+        print("done.")
+        return True
+    except: 
+        return False 
+
+class SRDataset(Dataset):
 
     def __init__(self, titles, abstracts, labels): 
         super(SRDataset).__init__()
@@ -36,16 +72,16 @@ class SRDataset():
         self.N = len(self.titles)
 
 
-    def __len__(self):
+    def __len__(self) -> int:
         return self.N
 
-    def __getitem__(self, index):
+    def __getitem__(self, index) -> tuple:
         ti = self.titles[index]
         abstract = self.abstracts[index]
         y = self.labels[index]
 
-        X = [ti + " [sep] " + abstract]
-        return X, y
+        X = ti + " [sep] " + abstract
+        return (X, y)
 
 
 
---
+++ simple_tests.py
@@ -0,0 +1,34 @@
+import numpy as np 
+
+import pandas as pd 
+
+import screening_model
+from screening_model import SRDataset
+
+
+import torch
+from torch.utils.data import IterableDataset, DataLoader
+
+
+def train_from_csv(review_path="example-datasets/labels_73.csv") -> None:
+    df = pd.read_csv(review_path)
+    df = df.replace(np.nan, "") 
+
+    # massage the labels to be binary only (0/1); first replace
+    # extant `0' values with `1' since these are maybes, then map
+    # `-1's to `0'. 
+    labels = df["level1_labels"].values  
+    labels = np.where(labels == 0, 1, labels)
+    labels = np.where(labels == -1, 0, labels)
+
+    dataset = SRDataset(df["title"].values[:100], 
+                        df["abstract"].values[:100], 
+                        labels[:100])
+    
+    success = screening_model.train_and_save(dataset, "1337") 
+    print(success)
+    #dl = DataLoader(dataset, batch_size=16)
+    #m = screening_model.train(dl)
+
+
+train_from_csv()
